3a9f2c8c6e9713fa5c68bef0e48778fb36d61c07
OMGToFo <93513240+OMGToFo@users.noreply.github.com>
2023-10-02 15:19:14 +0200

    2023.10.02 - MitFrameDownloads

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import cv2
 import numpy as np
 import tempfile
 
+import zipfile
+import os
+import base64
+
 def extract_stills(video_path, frame_interval):
     frames = []
     timecodes = []
@@ -33,6 +37,38 @@ def display_frames(frames, timecodes):
         # Display the frame using Streamlit
         st.image(frame, caption=f"Frame {i+1}, Timecode: {timecodes[i]:.2f} seconds")
 
+def download_link(frames, timecodes):
+    # Create a temporary directory to store the frames
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for i, (frame, timecode) in enumerate(zip(frames, timecodes)):
+            filename = f"frame_{i + 1}_timecode_{timecode:.2f}.png"
+            filepath = os.path.join(temp_dir, filename)
+            cv2.imwrite(filepath, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        
+        # Create a zip file containing all the frames
+        with zipfile.ZipFile("extracted_frames.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(temp_dir):
+                for file in files:
+                    zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), temp_dir))
+
+    # Provide a download link for the zip file
+    st.write("Download the zip file containing all frames:")
+    st.markdown(get_binary_file_downloader_link("extracted_frames.zip", "Click here to download"), unsafe_allow_html=True)
+
+
+
+
+def get_binary_file_downloader_link(file_path, download_link_text):
+    with open(file_path, 'rb') as file:
+        base64_file = base64.b64encode(file.read()).decode()
+    href = f'<a href="data:file/txt;base64,{base64_file}" download="{file_path}">{download_link_text}</a>'
+    return href
+
+
+
+
+
+
 
 st.title("Video to Stills Converter")
 
@@ -58,3 +94,8 @@ if uploaded_file is not None:
 
     # Display the selected frames with timecodes
     display_frames(frames, timecodes)
+
+
+    if st.button("Download All Frames"):
+        download_link(frames, timecodes)
+
